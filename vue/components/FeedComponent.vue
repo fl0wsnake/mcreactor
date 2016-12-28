0bbdfcdee7b343b7a0aabc7c89d28c6e56f573de
kirill <lastthursdayist@gmail.com>
2016-12-28 08:07:12 +0200

    Full moving to SPA succeded

--- vue/components/FeedComponent.vue
+++ vue/components/FeedComponent.vue
@@ -1,11 +1,12 @@
-
 <template>
-    <div class="col s8">
-        <template v-if="this.$store.state.user">
-            <new-post-component></new-post-component>
-        </template>
-        <post-list-component @submitted="loadPosts"></post-list-component>
-        <nav-component></nav-component>
+    <div class="row">
+        <div class="col s8">
+            <template v-if="this.$store.state.user">
+                <new-post-component @submitted="loadPosts"></new-post-component>
+            </template>
+            <post-list-component :posts="posts"></post-list-component>
+        </div>
+        <nav-component class="col s4"></nav-component>
     </div>
 </template>
 
@@ -16,10 +17,34 @@
 
     export default
     {
-        components:{
+        components: {
             NewPostComponent,
             PostListComponent,
             NavComponent
+        },
+        data(){
+            return {
+                posts: []
+            }
+        },
+        created() {
+            this.loadPosts()
+            if(this.$store.state.user)
+                this.$store.commit('loadSubscriptions')
+        },
+        methods: {
+            loadPosts(){
+                let path = this.$route.path
+                if(path == "/")
+                    path = 'post'
+                $.get(path, (posts) => {
+                    this.posts = posts
+                    console.log(this.posts)
+                })
+            }
+        },
+        watch: {
+            '$route': 'loadPosts'
         }
     }
 </script>
